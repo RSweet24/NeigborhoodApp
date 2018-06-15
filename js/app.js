e6dd2708c01214b2516a994a8d94c09262349a5d
Reuben <computergeek14@gmail.com>
2018-06-14 21:00:26 -0500

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,133 @@
+// Global Variables
+var map, clientID, clientSecret;
+
+function AppViewModel() {
+    var currentLocation = this;
+
+    this.searchOption = ko.observable("");
+    this.markers = [];
+
+    // This function populates the infowindow when the marker is clicked. We'll only allow
+    // one infowindow which will open at the marker that is clicked, and populate based
+    // on that markers position.
+    this.populateInfoWindow = function(marker, infowindow) {
+        if (infowindow.marker != marker) {
+            infowindow.setContent('');
+            infowindow.marker = marker;
+            // Foursquare API Client
+            clientID = "4BCYT21WBNSSAS4VW2QS5XDF4ZJXH3Y4W1JFGLMJ2L4WKEK2";
+            clientSecret = "3ZRSNTBHSRV3IGMEUBP4C1UITRLK0M3RNNEFLZD1I33SQHSD";
+            // URL for Foursquare API
+            var apiUrl = 'https://api.foursquare.com/v2/venues/search?ll=' +
+                marker.lat + ',' + marker.lng + '&client_id=' + clientID +
+                '&client_secret=' + clientSecret + '&query=' + marker.title +
+                '&v=20170708' + '&m=foursquare';
+            
+            // Foursquare API
+            $.getJSON(apiUrl).done(function(marker) {
+                var response = marker.response.venues[0];
+                currentLocation.street = response.location.formattedAddress[0];
+                currentLocation.city = response.location.formattedAddress[1];
+                currentLocation.zip = response.location.formattedAddress[3];
+                currentLocation.country = response.location.formattedAddress[4];
+                currentLocation.category = response.categories[0].shortName;
+
+                currentLocation.htmlContentFoursquare =
+                    '<div>' + '<h5 class="iw_subtitle">(' + currentLocation.category +
+                    ')</h5>' + '<div>' +
+                    '<h6 class="iw_address_title"> Address: </h6>' +
+                    '<p class="iw_address">' + currentLocation.street + '</p>' +
+                    '<p class="iw_address">' + currentLocation.city + '</p>' +
+                    '</div>' + '</div>';
+
+                infowindow.setContent(currentLocation.htmlContent + currentLocation.htmlContentFoursquare);
+            }).fail(function() {
+                // Send alert
+                alert(
+                    "There was an issue loading the Foursquare API. Please refresh your page to try again."
+                );
+            });
+
+            this.htmlContent = '<div>' + '<h4 class="iw_title">' + marker.title +
+                '</h4>' + '</div>';
+
+            infowindow.open(map, marker);
+
+            infowindow.addListener('closeclick', function() {
+                infowindow.marker = null;
+            });
+        }
+    };
+
+    this.populateAndBounceMarker = function() {
+        currentLocation.populateInfoWindow(this, currentLocation.largeInfoWindow);
+        this.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout((function() {
+            this.setAnimation(null);
+        }).bind(this), 1400);
+    };
+
+    this.initMap = function() {
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+            center: new google.maps.LatLng(37.422621, -122.083970),
+            zoom: 15,
+            styles: styles
+        };
+        // Constructor creates a new map - only center and zoom are required.
+        map = new google.maps.Map(mapCanvas, mapOptions);
+
+        // Set InfoWindow
+        this.largeInfoWindow = new google.maps.InfoWindow();
+        for (var i = 0; i < myLocations.length; i++) {
+            this.markerTitle = myLocations[i].title;
+            this.markerLat = myLocations[i].lat;
+            this.markerLng = myLocations[i].lng;
+            // Google Maps marker setup
+            this.marker = new google.maps.Marker({
+                map: map,
+                position: {
+                    lat: this.markerLat,
+                    lng: this.markerLng
+                },
+                title: this.markerTitle,
+                lat: this.markerLat,
+                lng: this.markerLng,
+                id: i,
+                animation: google.maps.Animation.DROP
+            });
+            this.marker.setMap(map);
+            this.markers.push(this.marker);
+            this.marker.addListener('click', currentLocation.populateAndBounceMarker);
+        }
+    };
+
+    this.initMap();
+
+    // This block appends our locations to a list using data-bind
+    // It also serves to make the filter work
+    this.myLocationsFilter = ko.computed(function() {
+        var result = [];
+        for (var i = 0; i < this.markers.length; i++) {
+            var markerLocation = this.markers[i];
+            if (markerLocation.title.toLowerCase().includes(this.searchOption()
+                    .toLowerCase())) {
+                result.push(markerLocation);
+                this.markers[i].setVisible(true);
+            } else {
+                this.markers[i].setVisible(false);
+            }
+        }
+        return result;
+    }, this);
+}
+
+googleError = function googleError() {
+    alert(
+        'Oops. Google Maps did not load. Please refresh the page and try again!'
+    );
+};
+
+function startApp() {
+    ko.applyBindings(new AppViewModel());
+}
---
+++ js/markers.js
@@ -0,0 +1,68 @@
+var myLocations = [
+    {
+        title: 'Googleplex',
+        lat: 37.422204,
+        lng:  -122.084047,
+        type: 'Google Headquarters'
+    },
+    {
+        title: 'In-N-Out Burger',
+        lat: 37.421122,
+        lng: -122.093357,
+        type: 'Fast-Food Burger'
+    },
+    {
+        title: 'Zareen\'s Restaurant',
+        lat: 37.416627,
+        lng: -122.079475,
+        type:  'Asian Restaurant'
+    },
+    {
+        title: 'Costco Wholesale',
+        lat: 37.421753,
+        lng: -122.095725,
+        type:  'Warehouse Store'
+    },
+    {
+        title: 'REI',
+        lat: 37.423722,
+        lng: -122.096432,
+        type: 'Camping Store'
+    },
+    {
+        title: 'Computer History Museum',
+        lat: 37.414911,
+        lng: -122.077421,
+        type: 'History Museum'
+    },
+    {
+        title: 'Century Cinema 16',
+        lat: 37.414848,
+        lng: -122.081131,
+        type: 'Movie Theater'
+    },
+    {
+        title: 'Peninsula Storage Center',
+        lat: 37.419348,
+        lng: -122.097568,
+        type: 'Self-Storage Facility'
+    },
+    {
+        title: 'Ross Dress for Less',
+        lat: 37.419921,
+        lng: -122.096035,
+        type: 'Clothing Store'
+    },
+    {
+        title: 'Orchard Supply hardware',
+        lat: 37.421483,
+        lng: -122.099824,
+        type: 'Hardware Store'
+    },
+    {
+        title: 'BP',
+        lat: 37.421847,
+        lng: -122.101436,
+        type: 'Gas Station'
+    }
+]
